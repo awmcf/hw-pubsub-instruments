88da94943b127c940bd470da19dfb4623dfdef9b
awmcf <aaronmcfaull@gmail.com>
2019-02-26 23:47:03 +0000

    Created pubsub in Instrument Families model

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
+const instrumentFamilyData = require('./data/instrument_family_data.js');
+const InstrumentFamilies = require('./models/instrument_families.js');
 const InstrumentFamilySelectView = require('./views/instrument_family_select_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
+  const instrumentFamiliesDataModel = new InstrumentFamilies(instrumentFamilyData);
+  instrumentFamiliesDataModel.bindEvents();
+
   const selectedInstrumentFamilyView = new InstrumentFamilySelectView();
   selectedInstrumentFamilyView.bindEvents();
 });
--- src/models/instrument_families.js
+++ src/models/instrument_families.js
@@ -1,5 +1,22 @@
+const PubSub = require('../helpers/pub_sub.js');
+
 const InstrumentFamilies = function(data) {
   this.data = data;
+};
+
+InstrumentFamilies.prototype.bindEvents = function() {
+  PubSub.subscribe('InstrumentFamilySelectView:instrument-selected', (evt) => {
+    const selectedInstrumentFamily = evt.detail;
+    const selectedInstrumentFamilyObject = this.findByName(selectedInstrumentFamily);
+    PubSub.publish('InstrumentFamily:selected-instrument-family-ready', selectedInstrumentFamilyObject);
+  });
+};
+
+InstrumentFamilies.prototype.findByName = function(searchName) {
+  const foundInstrumentFamily = this.data.find((currentInstrumentFamily) => {
+    return currentInstrumentFamily.name === searchName;
+  });
+  return foundInstrumentFamily;
 };
 
 module.exports = InstrumentFamilies;
